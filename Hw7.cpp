257e760100536c4298e875c1231cfa80cf7d903d
Jake Olsen <golddragon99@gmail.com>
2018-04-28 22:32:01 +0000

    random 1000 #

--- Hw7.cpp
+++ Hw7.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
 using namespace std;
 
 int main(){
-	cout<<"hi";
+	int seq[1000];
+	srand(time(0));
+	for(int i=0;i<1000;i++){
+		seq[i]=(rand()%250)+1;
+	}
 }
 
---
+++ build-dir/CMakeFiles/Hw7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hw7.dir/Hw7.cpp.o"
+  "Hw7.pdb"
+  "Hw7"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hw7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
